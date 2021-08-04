a1d06e4814c4d373ee4646841122ee1193872da1
Gyan Shresth <77232855+GYAN-2008@users.noreply.github.com>
2021-08-04 14:45:52 +0530

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,25 +1,25 @@
-const accessCode1 = "VARIABLE";
-const accessCode2 = "FUNCTION";
-const accessCode3 = "DATABASE";
+const accessCode1 = "WHITE";
+const accessCode2 = "JUNIOR";
+const accessCode3 = "HAT";
 
 function clues() {
     
     fill("white")
     textSize(15)
-    text("R E V B A I L A", 100,50)
+    text("I H W T E", 100,50)
     fill("lightblue")
-    text("Hint: Always changing, not constant !!", 100,70)
+    text("Hint: SIMILAR TO LIGHT !!", 100,70)
 // add code for displaying rest of the names and the hints.
     
 fill("white")
     textSize(15)
-    text("C U T N I F O N", 700,150)
+    text(" N J I U R O", 700,150)
     fill("lightblue")
-    text("Hint: Perfoms a particular task !!", 700,170)
+    text("Hint: SIMILAR TO NEWBIE !!", 700,170)
 
     fill("white")
-    textSize(15)
-    text("E T A D A S B A", 100,310)
+    textSize(15)                                                           
+    text("A T H", 100,310)
     fill("lightblue")
-    text("Hint: Stores all information !!", 100,330)
+    text("Hint: SAVES FROM HEAT !!", 100,330)
 }
